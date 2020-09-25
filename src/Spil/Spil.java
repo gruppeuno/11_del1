d6e77d8f991c5b43d984309f3210b69d90745da5
Kristoffer <kristoffer@kritix.dk>
2020-09-25 14:50:27 +0200

    Opdateret håndtering af data

--- src/Spil/Spil.java
+++ src/Spil/Spil.java
@@ -5,7 +5,16 @@ public class Spil {
     public static void main(String[] args) {
 
         Terning t1 = new Terning(1);
-        t1.kast();
-        System.out.println(t1);
+        Terning t2 = new Terning(1);
+
+        t1.kast(); t2.kast();
+
+        //Omdanner Terning class output til int
+        int kast1 = Integer.parseInt(String.valueOf(t1));
+        int kast2 = Integer.parseInt(String.valueOf(t2));
+
+        System.out.println(kast1);
+        System.out.println(kast2);
+
     }
 }
