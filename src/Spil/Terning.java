aa42e7098c67ef6520007d87a7ef917e67e2a714
Kristoffer <kristoffer@kritix.dk>
2020-09-25 14:04:54 +0200

    Lavet class til terning

---
+++ src/Spil/Terning.java
@@ -0,0 +1,24 @@
+package Spil;
+
+import java.util.Random;
+
+public class Terning {
+    private final int MAKS = 6;
+
+    private int tVaerdi;
+
+    public Terning (int vaerdi) {
+        tVaerdi = vaerdi;
+    }
+
+    public void kast () {
+        Random tilf = new Random();
+        tVaerdi = tilf.nextInt(MAKS)+1;
+    }
+    public void setTVaerdi(int vaerdi) {
+        tVaerdi = vaerdi;
+    }
+    public int getTVaerdi() {
+        return tVaerdi;
+    }
+}
--- src/Test/Test.java
+++ src/Test/Test.java
@@ -3,6 +3,6 @@ package Test;
 public class Test {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
