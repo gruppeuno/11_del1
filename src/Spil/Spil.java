f211a769e9be2df8f67438eb5f8a84c28761989d
Kristoffer <kristoffer@kritix.dk>
2020-09-27 00:11:28 +0200

    Opdateret og testet "reglerne" (if statements) i metoden for spillet.

--- src/Spil/Spil.java
+++ src/Spil/Spil.java
@@ -6,7 +6,6 @@ public class Spil {
 
     public static void main(String[] args) {
 
-
         //Terning objekt bliver lavet for t1 og t2
         Terning t1 = new Terning(1);
         Terning t2 = new Terning(2);
@@ -30,13 +29,8 @@ public class Spil {
             //Spillet for spiller1
             point1 = kastText(t1, t2, scan, spiller1, point1);
 
-
-
             //Spillet for spiller2
             point2 = kastText(t1, t2, scan, spiller2, point2);
-
-
-
         }
     }
 
@@ -47,7 +41,7 @@ public class Spil {
         String tKast;
 
         do {
-            System.out.println("Brug kommandoen \"kast\"");
+            System.out.println("Brug kommandoen \"Kast\"");
 
             tKast = scan.nextLine();
         }
@@ -64,19 +58,22 @@ public class Spil {
         System.out.println(kast1);
         System.out.println(kast2);
 
-        if (kast1 == 1 && kast2 == 1) {
-            kast1 = 0; kast2 = 0; point = 0;
-        }
-        else if (point > 40 && kast1 == 6 && kast2 == 6) {
-            System.out.println("Tilykke!" + spiller + " har vundet spillet!");
+        //Tjekker om spilleren har vundet
+        if (point > 40 && kast1 == kast2) {
+            System.out.println("Tilykke! " + spiller + " har vundet spillet!");
 
             System.exit(0);
+        }
+        //Checker om der er slået to 1 taller.
+        else if (kast1 == 1 && kast2 == 1) {
+            kast1 = 0; kast2 = 0; point = 0;
         }
 
         point = kast1 + kast2 + point;
 
         System.out.println(spiller + " har nu: "+ point);
 
+        //Tjekker om der skal slåes igen.
         if (kast1 == kast2) {
             System.out.println(spiller + " har slået dobbelt. Du får en ekstra tur!");
             point = kastText(t1, t2, scan, spiller, point);
