c778a19f141492b4c3c3af173ea5c19df9fb1fc0
Kristoffer <kristoffer@kritix.dk>
2020-09-28 18:04:15 +0200

    Rewritten the text and comments to english

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -6,20 +6,20 @@ public class Game {
 
     public static void main(String[] args) {
 
-        //Terning objekt bliver lavet for t1 og t2
+        //Dice object gets created. Two instances.
         Dice t1 = new Dice(1);
         Dice t2 = new Dice(2);
 
         Scanner scan = new Scanner(System.in);
 
-        //Opretter spiller1 og spiller2.
-        System.out.println("Vælg navn: Spiller 1");
+        //Creates player1 and player2
+        System.out.println("Choose name: Player 1");
         String player1 = scan.nextLine();
-        System.out.println("Vælg navn: Spiller 2");
+        System.out.println("Choose name: Player 2");
         String player2 = scan.nextLine();
 
         while (player1.toLowerCase().equals(player2.toLowerCase())) {
-            System.out.println("Spiller 1 og 2 kan ikke have det samme navn - Vælg nyt navn til Spiller 2");
+            System.out.println("Player 1 and Player 2 cannot have same name - Player 2 please choose another name");
             player2 = scan.nextLine();
         }
 
@@ -27,12 +27,12 @@ public class Game {
 
         while (true) {
 
-            //Spillet for spiller1
+            //Game for player1
             point1 = GameEN.rollText(t1, t2, player1, point1);
 
             if (point1 == -1) {break;}
 
-            //Spillet for spiller2
+            //Game for player2
             point2 = GameEN.rollText(t1, t2, player2, point2);
 
             if (point2 == -1) {break;}
--- src/Game/GameEN.java
+++ src/Game/GameEN.java
@@ -19,36 +19,34 @@ public class GameEN {
         }
         while (!diceRoll.toLowerCase().equals("roll"));
 
-        //Kaster terningerne
+        //Rolls the dice
         t1.roll();
         t2.roll();
 
-        //Laver terningerne om til int.
+        //Reassigns the string value of t1 and t2 to int.
         int roll1 = Integer.parseInt(String.valueOf(t1));
         int roll2 = Integer.parseInt(String.valueOf(t2));
 
         System.out.println(roll1);
         System.out.println(roll2);
-        System.out.println("Terningsum i alt for tur: " + roll1 + roll2);
+        System.out.println("Total sum of the two dices for this round: " + roll1 + roll2);
 
-        //Tjekker om spilleren har vundet
+        //Checks if player has won
         if (point > 40 && roll1 == roll2) {
-            System.out.println("Tilykke! " + player + " har vundet spillet!");
+            System.out.println("Congratulations!\n " + player + " has won the game!");
 
             return -1;
         }
-        //Checker om der er slået to 1 taller.
+        //Checking the value of the dices to see if there are multiple 1's.
         else if (roll1 == 1 && roll2 == 1) {
-            roll1 = 0;
-            roll2 = 0;
-            point = 0;
+            roll1 = 0; roll2 = 0; point = 0;
         }
 
         point = roll1 + roll2 + point;
 
         System.out.println(player + " har nu: " + point);
 
-        //Tjekker om der skal slåes igen.
+        //Checking for reroll
         if (roll1 == roll2) {
             System.out.println(player + " har slået dobbelt. Du får en ekstra tur!");
             point = rollText(t1, t2, player, point);
