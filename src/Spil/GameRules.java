abff9238baff7cdcf8662e51acd17615479577f8
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2020-09-29 12:18:09 +0200

    tæt på done, implementeret kris scan til at afvente roll, spille kører med krav, der mangler dog at fixe et par bugs

--- src/Spil/GameRules.java
+++ src/Spil/GameRules.java
@@ -9,8 +9,10 @@ public class GameRules {
     public void Game(int die1, int die2, Player p) {
         System.out.println(p.getPlayerName() + " slog " + die1 +" og " + die2);
 
-        if (p.getPoints() > MAX) {
+        if ((p.getPoints() + die1 + die2)>= MAX) {
             p.setPlayerPendingWin(true);
+            p.setPoints(MAX);
+
         } else {
             p.setPoints(p.getPoints() + die1 + die2);
         }
@@ -23,6 +25,7 @@ public class GameRules {
             if ((die1 == 1 && die2 == 1)) {
                 p.setPoints(0);
                 p.setPlayerPendingWin(false);
+                System.out.println("du slog to 1'ere og mistede alle dine points... Du må slå igen");
             }
 
             //vinder hvis man slår 2 seksere i streg, uanset om det var samme eller forrige tur
@@ -30,19 +33,31 @@ public class GameRules {
                 //hvis man slog 2x6 sidste tur
                 if (p.getAntalSeksere() == 1) {
                     p.setPlayerWin(true);
+                    System.out.println("du slog to 6'ere igen!");
                 }
                 //sætter antal 2x6 til 1
                 else {
                     p.setAntalSeksere(1);
+                    System.out.println(p.getPlayerName() + " har nu " + p.getPoints() + " points");
                 }
             }
             //slå 2 ens for at vinde, hvis altså du er på 40 points
             if (p.getPlayerPendingWin() == true) {
                 p.setPlayerWin(true);
+                System.out.println("Du vandt!");
+                p.setEkstraSlag(false);
             }
-        } else {
+
+            else {
+                p.setAntalSeksere(0);
+                System.out.println(p.getPlayerName() + " har nu " + p.getPoints() + " points");
+            }
+        }
+
+        else {
             p.setEkstraSlag(false);
             p.setAntalSeksere(0);
+            System.out.println(p.getPlayerName() + " har nu " + p.getPoints() + " points");
         }
     }
 }
--- src/Spil/main.java
+++ src/Spil/main.java
@@ -5,20 +5,40 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
 
+        boolean winner = false;
         GameRules newGame = new GameRules();
         Player player1 = new Player();
         Player player2 = new Player();
         RaffleCup cup = new RaffleCup();
         player1.setPlayerName("gab");
         player2.setPlayerName("Dennis");
+        Scanner scan = new Scanner(System.in);
 
 
-        cup.roll();
-        newGame.Game(cup.getDie1(),cup.getDie2(), player1);
+        while (player1.getPlayerWin()==false && player2.getPlayerWin()==false){
+            do {
+
+                //String diceRoll;
+                //do {
+                //    System.out.println(player1.getPlayerName() + " use the command \"Roll\"");
+//
+                //    diceRoll = scan.nextLine();
+                //}
+                //while (!diceRoll.toLowerCase().equals("roll"));
+
+                cup.roll();
+                newGame.Game(cup.getDie1(),cup.getDie2(), player1);
+            }while (player1.getEkstraSlag()!=false && player2.getPlayerWin()!=true);
+
+            if (player1.getPlayerWin()==true){
+                break;
+            }
+            System.out.println();
+            System.out.println("næste spillers tur");
+            System.out.println();
 
 
-        cup.roll();
-        newGame.Game(cup.getDie1(),cup.getDie2(), player2);
+        }
 
 
     }
