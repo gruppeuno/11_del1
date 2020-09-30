32cb85b55d86226657844cda2323c6856b246350
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2020-09-30 15:43:30 +0200

    allernyeste beach babe

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,6 +2,13 @@ package Test;
 
 public class Test {
     public static void main(String[] args) {
+        int t = 0;
+        int resultat = 0;
 
+        while(t<1000){
+            resultat+=(int)(Math.random() * 6) + 1;
+            t++;
+        }
+        System.out.println(resultat);
     }
 }
