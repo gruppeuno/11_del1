0350917b2dd28be7601b89263ea2bbb401fdb67a
Kristoffer <kristoffer@kritix.dk>
2020-10-01 20:22:40 +0200

    Opdateret tekst

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -17,6 +17,7 @@ public class Test {
             System.out.println(i + ": " + die1);
             DiceResult[i] = die1;
         }
+        //Output af min max, og sum total
         System.out.println("\nStørste tal i array: " + Arrays.stream(DiceResult).max().getAsInt());
         System.out.println("Mindste tal i array: " + Arrays.stream(DiceResult).min().getAsInt());
         System.out.println("\n\nTotale sum af alle kast: " + Arrays.stream(DiceResult).sum());
