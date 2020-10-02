7d1c9d21fb3bfb13c596e53f7a38de7c3e1c04f7
Christian Pedersen <chrpedersen861@gmail.com>
2020-10-02 10:44:18 +0200

    Christian´s Commit

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -1,7 +1,7 @@
 package Game;
 
 public class Game {
-
+     // Christian Commit til master"Den skal være negativ"
     //Gabriels commit til master: “Hvis du ikke lever livet hurtigt, så lever du livet langsomt🏎”
     //game controller skal være static for at fungere i static main
     static GameController ny = new GameController();
